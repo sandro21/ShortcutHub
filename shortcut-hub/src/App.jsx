741a2a07297cef9adbe61b2ca70443aa51433800
Sandro <sandroqarqusashvili@gmail.com>
2025-04-11 22:33:11 -0400

    ds

--- shortcut-hub/src/App.jsx
+++ shortcut-hub/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { categories } from './data';
 import './index.css';
 import ToolPage from './pages/ToolPage';
@@ -73,7 +73,6 @@ function Homepage() {
 function App() {
   return (
     <Router basename="/ShortcutHub">
-
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/tools/:toolName" element={<ToolPage />} />
