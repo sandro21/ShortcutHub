eaf90a5116f02b388d80b915ba2cb84310a49331
Sandro <sandroqarqusashvili@gmail.com>
2025-04-15 10:30:40 -0400

    go back 1 version after adding picctures and data

--- src/data.js
+++ src/data.js
@@ -39,31 +39,8 @@ export const categories = [
       { name: 'Slack', link: '/slack' },
       { name: 'Google Docs', link: '/google-docs' },
       { name: 'Google Sheets', link: '/google-sheets' },
-      { name: 'Apple Calendar', link: '/apple-calendar' },
-      { name: 'Chrome', link: '/chrome' },
-      { name: 'Gmail', link: '/gmail' },
-      { name: 'Google Calendar', link: '/google-calendar' },
-      { name: 'Google Meet'},
-        { name: 'Microsoft Teams', link: '/microsoft-teams' },
-        { name: 'Safari', link: '/safari' },
-        { name: 'Zoom', link: '/zoom' },
-      ],
-  },
-  {
-    name: 'Operating Systems',
-    tools: [
-        { name: 'macOS', link: '/macos' },
-        { name: 'Windows', link: '/windows' },
-        { name: 'Ubuntu', link: '/ubuntu' },
-      ],
-    },
-    {
-      name: '3D',
-      tools: [
-        { name: 'Blender', link: '/blender' }, // Known for extensive shortcuts
-        { name: 'Cinema 4D', link: '/cinema-4d' }, // Known for shortcuts
-      ],
-    },
+    ],
+  }
   ];
 
 
